da450c0320cb50f1453eb2bbb7fdb04dff56196d
Helena Jakobsen <helenajakobsen@helenas-air-4.lan>
2021-02-09 09:26:07 +0100

    filtrering er færdig

--- afleveringer/babushka/script.js
+++ afleveringer/babushka/script.js
@@ -1,7 +1,3 @@
-document.addEventListener("DOMContentLoaded", start);
-let menu;
-
-
 const url = "https://babushka-dd8a.restdb.io/rest/menu";
 const medieurl = "https://babushka-dd8a.restdb.io/media/"
 const options = {
@@ -10,27 +6,49 @@ const options = {
     }
 };
 
+document.addEventListener("DOMContentLoaded", start);
+let menu;
+let filter = "alle";
 
 function start() {
+    const filterKnapper = document.querySelectorAll("nav button");
+    filterKnapper.forEach(knap => knap.addEventListener("click", filtrerKategori));
     hentData();
+}
+const header = document.querySelector("header h2");
+
+function filtrerKategori() {
+    filter = this.dataset.kategori;
+    document.querySelector(".valgt").classList.remove("valgt");
+    this.classList.add("valgt");
+    visMenu(); //kalder funktionen visPersoner efter det nye filter er sat
+    header.textContent = this.textContent;
+    console.log(this);
+
 }
 async function hentData() {
     const resultat = await fetch(url, options);
     menu = await resultat.json();
+    console.log("menu", menu);
     visMenu();
 }
 
 function visMenu() {
     console.log(menu);
-    const main = document.querySelector("main");
+    const dest = document.querySelector("main");
+    // const main = document.querySelector("main");
     const template = document.querySelector("template").content;
+    dest.textContent = "";
+
     menu.forEach(menu => {
-        const klon = template.cloneNode(true);
-        klon.querySelector(".billede").src = medieurl + menu.billede;
-        klon.querySelector(".navn").textContent = menu.navn;
-        klon.querySelector(".kategori").textContent = menu.kategori;
-        klon.querySelector(".kortbeskrivelse").textContent = menu.kortbeskrivelse;
-        klon.querySelector(".pris").textContent = `Pris: ${menu.pris}`;
-        main.appendChild(klon);
+        if (filter == menu.kategori || filter == "alle") {
+            const klon = template.cloneNode(true);
+            klon.querySelector(".billede").src = medieurl + menu.billede;
+            klon.querySelector(".navn").textContent = menu.navn;
+            klon.querySelector(".kategori").textContent = menu.kategori;
+            klon.querySelector(".kortbeskrivelse").textContent = menu.kortbeskrivelse;
+            klon.querySelector(".pris").textContent = `Pris: ${menu.pris}`;
+            dest.appendChild(klon);
+        }
     })
 }
